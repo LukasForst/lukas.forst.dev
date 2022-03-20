36568705b86dbd05e7250b80bc547a88173be15c
Lukas Forst <lukas@forst.dev>
2022-03-20 18:08:43 +0100

    add more granular og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,8 @@ import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const title = 'Lukas Forst';
-  const description = 'Lukas Forst - Senior Software Engineer, Co-founder Mild Blue, R&D at Wire, CyberSec at CTU, Research at StratosphereIPS';
+  const description = 'Senior Software Engineer, Co-founder Mild Blue, R&D at Wire, CyberSec at CTU, Research at Stratosphere IPS.';
+  const url = 'https://lukas.forst.dev';
   return (
     <>
       <Head>
@@ -18,15 +19,18 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
         {/* Graph data */}
+        <meta property="og:url" content={url}/>
         <meta property="og:title" content={title}/>
         <meta property="og:type" content="website"/>
         <meta property="og:description" content={description}/>
         <meta
           property="og:image"
-          content="/images/thumbnail.jpeg"
+          content="/images/thumbnail.jpg"
         />
         {/* Twitter */}
         <meta name="twitter:site" content="@lukas-forst"/>
+        <meta property="twitter:domain" content={url.replace('https://', '')}/>
+        <meta property="twitter:url" content={url}/>
         <meta name="twitter:title" content={title}/>
         <meta name="twitter:description" content={description}/>
         <meta name="twitter:card" content="summary || summary_large_image || player || app"/>
