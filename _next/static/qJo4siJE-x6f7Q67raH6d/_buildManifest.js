99855eb4ab5cac5f63728ad9ab3055e7be87afb2
LukasForst <lukasforst+github@gmail.com>
2022-07-04 09:10:38 +0000

    Deploying to gh-pages from @ LukasForst/lukas.forst.dev@e7f9208fd03b875d45e38a50bc4af3fd501b2e28 🚀

--- _next/static/qJo4siJE-x6f7Q67raH6d/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c138d6fdc42b98e8.css","static/chunks/pages/index-0c0362f364f2fbc1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
